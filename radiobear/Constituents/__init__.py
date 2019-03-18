ccf59e4fef1c9a731343a63f7659e1b99370c7b8
ddeboer <ddeboer@berkeley.edu>
2019-03-18 07:03:38 -0700

    a little bit of cleanup

--- radiobear/Constituents/__init__.py
+++ radiobear/Constituents/__init__.py
@@ -2,10 +2,10 @@
 # Copyright 2018 the HERA Collaboration
 # Licensed under the 2-clause BSD license.
 
-from . import clouds
-from . import co
-from . import h2
-from . import h2o
-from . import h2s
-from . import nh3
-from . import ph3
+# from . import clouds
+# from . import co
+# from . import h2
+# from . import h2o
+# from . import h2s
+# from . import nh3
+# from . import ph3
