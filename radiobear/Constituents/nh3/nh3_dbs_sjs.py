1f5a466e909259819a9c1962db191e842792a7f7
ddeboer <ddeboer@berkeley.edu>
2019-03-19 09:07:05 -0700

    added a bit o doc

--- radiobear/Constituents/nh3/nh3_dbs_sjs.py
+++ radiobear/Constituents/nh3/nh3_dbs_sjs.py
@@ -1,9 +1,11 @@
 from radiobear.Constituents.nh3 import nh3_sjs
 from radiobear.Constituents.nh3 import nh3_dbs
-import numpy as np
 
 
 def alpha(freq, T, P, X, P_dict, otherPar, units='dBperkm', path='./', verbose=False):
+    """
+    This calls the appropriate formalism depending on the pressure.
+    """
     PLower = 400.0
     PMid = 800.0
     PHigher = 2000.0
@@ -13,8 +15,8 @@ def alpha(freq, T, P, X, P_dict, otherPar, units='dBperkm', path='./', verbose=F
     elif P > PHigher:
         alpha_nh3 = nh3_sjs.alpha(freq, T, P, X, P_dict, otherPar, units, path, verbose)
     else:
-        a2 = np.array(nh3_sjs.alpha(freq, T, P, X, P_dict, otherPar, units, path, verbose))
-        a1 = np.array(nh3_dbs.alpha(freq, T, P, X, P_dict, otherPar, units, path, verbose))
+        a2 = nh3_sjs.alpha(freq, T, P, X, P_dict, otherPar, units, path, verbose)
+        a1 = nh3_dbs.alpha(freq, T, P, X, P_dict, otherPar, units, path, verbose)
         if wtype == 'exp':
             W = np.exp(-(P - PMid)**2 / 1000.0)
         else:
