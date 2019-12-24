29b301503c92964d6506b96f5051c303263fc285
David DeBoer <ddeboer@berkeley.edu>
2019-12-24 12:32:09 -0800

    missed one

--- radiobear/plotting/bright.py
+++ radiobear/plotting/bright.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from radiobear import utils
-import six
 
 
 # ##############################################################################################################
