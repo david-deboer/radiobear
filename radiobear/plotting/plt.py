9d9a16be537295c95938bedd00813a311c0d00d5
David DeBoer <ddeboer@berkeley.edu>
2019-12-30 12:51:05 -0800

    removed errant b plt

--- radiobear/plotting/plt.py
+++ radiobear/plotting/plt.py
@@ -45,44 +45,6 @@ def Tb(fn=None, xaxis='Frequency', directory='Output', file_type='spectrum', **k
     return fio
 
 
-def b(fn=None, xaxis='Frequency', xlog=False, directory='Output', distance=4377233696.68):
-    """
-    <<<NOT READY>>>
-    plots brightness temperature against frequency and disc location:
-           fn = filename to read (but then ignores directory) | '?', '.' or None | integer [None]
-           xaxis = 'f[requency]' | 'w[avelength' ['freq']
-           xlog = True | False [False]
-           justFreq = True | False [False]
-           directory = subdirectory for data (not used if filename given) ['Output']
-           distance = distance for angular size plot in km [4377233696 km for Neptune]"""
-    # # b plot
-    fio = fileIO.FileIO(directory=directory)
-    fio.read(fn=fn, file_type='spectrum')
-    plt.figure('b')
-    for i in range(len(f)):
-        plt.plot(b[:, 0], Tb[:, i], label=str(f[i]))
-        plt.plot(b[:, 0], Tb[:, i], 'o')
-    plt.legend()
-    plt.title(title[-1])
-    plt.xlabel('km')
-    plt.ylabel('Brightness Temperature [K]')
-
-    # b plot vs angle
-    angle = []
-    for r in b[:, 0]:
-        angle.append((r / distance) * (180.0 / np.pi) * 3600.0)
-    plt.figure('b_vs_angle')
-    for i in range(len(f)):
-        plt.plot(angle, Tb[:, i], label=str(f[i]))
-        plt.plot(angle, Tb[:, i], 'o')
-    plt.legend()
-    plt.title(title[-1])
-    plt.xlabel('arcsec')
-    plt.ylabel('Brightness Temperature [K]')
-
-    return len(b)
-
-
 def Obs(fn, cols=[0, 1, 2], color='b', marker='o', delimiter=None, comline='!'):
     """
     <<<<????>>>>
