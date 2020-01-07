031441b2d2e31516c27794b32be243b78f4297c2
David DeBoer <ddeboer@berkeley.edu>
2020-01-06 20:47:19 -0800

    that worked out well!

--- radiobear/raypath.py
+++ radiobear/raypath.py
@@ -145,14 +145,10 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
     r = [geoid.r]
     t_inc = [np.arccos(-np.dot(s[-1], n[-1]))]            # incident angle_0
     nratio = nr[0] / nr[1]                                # refractive index ratio_0
-    t_tran = [np.arcsin(nratio * np.sin(t_inc[-1]))]    # transmitted angle_0
+    t_tran = [np.arcsin(nratio * np.sin(t_inc[-1]))]      # transmitted angle_0
 
     # return array initialization, use a value to keep indexing numbering for loop
-    ds = [0.0]
-    layer4ds = [0.0]
-    r4ds = [0.0]  # not needed, but for information
-    P4ds = [0.0]  # not needed, but for information
-    doppler = []
+    path.update(ds=[0.0], layer4ds=[0.0], r4ds=[0.0], P4ds=[0.0], doppler=[])
 
     # loop while in atmosphere
     i = 0                      # loops over the path
@@ -164,7 +160,7 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
             print('------------------')
             print('\tstep {}:  layer {} {} '.format(i, layer, direction))
             print('\ts = [{:.4f}, {:.4f}, {:.4f}],  ds = {:.4f}'.
-                  format(s[-1][_X], s[-1][_Y], s[-1][_Z], ds[-1]))
+                  format(s[-1][_X], s[-1][_Y], s[-1][_Z], path.ds[-1]))
             geoid.print_a_step()
             print('\tt_inc, tran:  {:.8f} -> {:.8f}'.
                   format(utils.r2d(t_inc[-1]), utils.r2d(t_tran[-1])))
@@ -176,8 +172,8 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
         rdots = np.dot(r[i], s[i + 1])
 
         vw = np.interp(pclat, atm.config.vwlat, atm.config.vwdat) / 1000.0
-        dopp = (1.0 - (atm.config.omega_m * rNowMag * np.cos(utils.d2r(pclat)) + vw) *
-                np.sin(utils.d2r(delta_lng)) / 3.0E5)
+        path.doppler.append(1.0 - (atm.config.omega_m * rNowMag * np.cos(utils.d2r(pclat)) + vw) *
+                            np.sin(utils.d2r(delta_lng)) / 3.0E5)
 
         # get ds, checking for exit, errors and warnings...
         try:
@@ -203,7 +199,7 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
         # What is correct here?  Is this true for egress, or only in error?
         # 8/1/14 I commented out if statement, but only print statement was indented
         if ds_step < 0.0:
-            print('Error:  ds < 0  ({}:  r.s={}, ds={}, [{},{}])'
+            print('Warning:  ds < 0  ({}:  r.s={}, ds={}, [{},{}])'
                   .format(direction, rdots, ds_step, dsp, dsm))
             inAtmosphere = False
             break
@@ -212,14 +208,13 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
             ds_step = abs(rNextMag - rNowMag) / mu
 
         # append to return arrays
-        ds.append(ds_step)
-        layer4ds.append(layer)
-        r4ds.append(rNowMag)
-        P4ds.append(atm.gas[atm.config.C['P']][layer])
-        doppler.append(dopp)
+        path.ds.append(ds_step)
+        path.layer4ds.append(layer)
+        path.r4ds.append(rNowMag)
+        path.P4ds.append(atm.gas[atm.config.C['P']][layer])
 
         # get next step, double-check r value (and compute n, etc)
-        rnext = r[i] + ds[i + 1] * s[i + 1]
+        rnext = r[i] + path.ds[i + 1] * s[i + 1]
         pclat = utils.r2d(np.arcsin(np.dot(rnext, yHat) / np.linalg.norm(rnext)))
         delta_lng = utils.r2d(np.arctan2(np.dot(rnext, xHat), np.dot(rnext, zHat)))
         r2 = geoid.calcShape(atm, req[layer + raypathdir[direction]], pclat, delta_lng)
@@ -259,53 +254,9 @@ def compute_ds(atm, b, orientation=None, gtype=None, verbose=False):
         # ## end loop ###
 
     # Get rid of the first entry, which was just used to make indexing in loop consistent
-    del ds[0], layer4ds[0], r4ds[0], P4ds[0]
-    # dsmu = []
-    # for i in range(min(len(ds), len(r4ds)) - 1):
-    #     if abs(r4ds[i] - r4ds[i + 1]) < 1E-6:
-    #         dsmuappend = 0.001
-    #     else:
-    #         dsmuappend = ds[i] / (r4ds[i] - r4ds[i + 1])
-    #     dsmu.append(dsmuappend)
-    path.update(ds=ds, layer4ds=layer4ds, r4ds=r4ds, P4ds=P4ds, doppler=doppler,
-                tip=tip, rotate=rotate, rNorm=rNorm)
+    del path.ds[0], path.layer4ds[0], path.r4ds[0], path.P4ds[0]
 
-    del s, r, n, ds, layer4ds, r4ds, P4ds, geoid, req, nr
+    path.update(tip=tip, rotate=rotate, rNorm=rNorm)
+
+    del s, r, n, geoid, req, nr
     return path
-
-
-# ##Test functions
-def refractTest(layers):
-    """Returns fake refractive index at layers"""
-    n = [1.0]
-    n = []
-    for r in layers:
-        v = 1.0 + (3000.0 / r)**2
-        n.append(v)
-    return n
-
-
-def computeEdges(z):  # not used anymore
-    edge = [(3.0 * z[0] - z[1]) / 2.0]
-    for i in range(len(z) - 1):
-        edge.append((z[i] + z[i + 1]) / 2.0)
-    edge.append((3.0 * z[-1] - z[-2]) / 2.0)
-    return edge
-
-
-def layersTest(rmin=100.0, rmax=20000.0, nlyr=100):
-    """Returns layer"""
-    dr = (rmax - rmin) / nlyr
-    mid = []
-    for i in range(nlyr):
-        r = rmax - i * dr
-        mid.append(r)
-    return mid
-
-
-def testPath(b=0.5, rmin=12000.0, rmax=20000.0, nlyr=50, verbose=False):
-    # make layers
-    mid = layersTest(rmin=rmin, rmax=rmax, nlyr=nlyr)
-    n = refractTest(mid)
-    ds = compute_ds(b, mid, n, verbose=verbose)
-    return mid, ds
